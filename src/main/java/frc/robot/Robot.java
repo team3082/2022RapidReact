c8bb391de9a3a61e07d70bee53edcf8760f78824
team3082 <minnetonka3082@gmail.com>
2022-01-29 13:27:58 -0600

    Add Swerve Modules

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,26 +1,48 @@
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.TimedRobot;
+import frc.robot.subsystems.SwerveMod;
 
 public class Robot extends TimedRobot {
 
+  final double ticksPerRot = 2048*12.8;
+  Joystick m_joystick;
+
+  SwerveMod swerve;
+
   @Override
-  public void robotInit() {}
+  public void robotInit() {
+    m_joystick = new Joystick(0);
+    swerve = new SwerveMod(2, 4);
+  }
 
   @Override
   public void robotPeriodic() {}
 
   @Override
-  public void autonomousInit() {}
-
-  @Override
-  public void autonomousPeriodic() {}
-
-  @Override
   public void teleopInit() {}
 
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+
+    double x = m_joystick.getRawAxis(0);
+    double y = -1* m_joystick.getRawAxis(1);
+    double drive = Math.hypot(x, y);
+
+    if(drive > 0.1){
+      swerve.drive(0.3 * drive);
+      double joystickPos;
+      joystickPos = ticksPerRot/2 + Math.atan2(y, x) / (2 * Math.PI) * ticksPerRot;
+      swerve.rotate(joystickPos);
+    }
+  }
+
+  @Override
+  public void testInit() {}
+
+  @Override
+  public void testPeriodic() {}
 
   @Override
   public void disabledInit() {}
@@ -29,8 +51,9 @@ public class Robot extends TimedRobot {
   public void disabledPeriodic() {}
 
   @Override
-  public void testInit() {}
+  public void autonomousInit() {}
 
   @Override
-  public void testPeriodic() {}
+  public void autonomousPeriodic() {}
 }
+  
---
+++ src/main/java/frc/robot/subsystems/SwerveMod.java
@@ -0,0 +1,68 @@
+package frc.robot.subsystems;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.TalonFXFeedbackDevice;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+
+public class SwerveMod {
+
+    private static final double ticksPerRot = 2048*12.8;
+    
+    private TalonFX m_steer; 
+    private TalonFX m_drive;
+
+    private boolean inverted;
+
+    public SwerveMod(int steerId, int driveId){
+        m_steer = new TalonFX(steerId);
+        m_drive = new TalonFX(driveId);
+        // configure encoders/PID
+        m_steer.configFactoryDefault();
+        m_drive.configFactoryDefault();
+
+        m_steer.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 20);
+        //m_steer.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.QuadEncoder, 0, 20);
+
+        m_steer.config_kF(0, 0, 10);
+		m_steer.config_kP(0, 0.3, 10);
+		m_steer.config_kI(0, 0, 10);
+		m_steer.config_kD(0, 0, 10);
+    }
+    
+    public void drive(double power){
+        m_drive.set(TalonFXControlMode.PercentOutput, power * (inverted?-1.0:1.0));
+    }
+
+    //rotates to a position given in ticks
+    public void rotate(double angle){
+        double motorPos = m_steer.getSelectedSensorPosition();
+    
+        int numRot = (int)Math.floor(motorPos/ticksPerRot);
+        double joystickTarget = numRot*ticksPerRot+angle;
+        double joystickTargetPlus = joystickTarget + ticksPerRot;
+        double joystickTargetMinus = joystickTarget - ticksPerRot;
+        
+        double destination;
+    
+        if(Math.abs(joystickTarget-motorPos)<Math.abs(joystickTargetPlus-motorPos) && Math.abs(joystickTarget-motorPos)<Math.abs(joystickTargetMinus-motorPos)){
+            destination = joystickTarget;
+        } else if(Math.abs(joystickTargetPlus-motorPos)<Math.abs(joystickTargetMinus-motorPos)){
+            destination = joystickTargetPlus;
+        } else {
+            destination = joystickTargetMinus;
+        }
+
+        if(Math.abs(destination-motorPos) > ticksPerRot/4.0){
+            inverted = true;
+            if(destination>motorPos)
+                destination -= ticksPerRot/2.0;
+            else
+                destination += ticksPerRot/2.0;
+        } else {
+            inverted = false;
+        }
+
+        m_steer.set(TalonFXControlMode.Position, destination);
+    }
+
+}
