e22c413d42b5b88a96c8a4b4ecd1c683fc6d8499
Ryan the Code Wizard 324 <proCoderRyan@googleSoftwareEngineer.dev>
2022-02-08 15:39:22 -0600

    Added motion magic to swerve Mod, not tested

--- src/main/java/frc/robot/subsystems/SwerveMod.java
+++ src/main/java/frc/robot/subsystems/SwerveMod.java
@@ -8,7 +8,7 @@ public class SwerveMod {
 
     private static final double ticksPerRot = 2048*12.8;
     
-    private TalonFX m_steer; 
+    private TalonFX m_steer;
     private TalonFX m_drive;
 
     private boolean inverted;
@@ -23,14 +23,33 @@ public class SwerveMod {
         m_steer.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 20);
         //m_steer.configSelectedFeedbackSensor(TalonSRXFeedbackDevice.QuadEncoder, 0, 20);
 
-        m_steer.config_kF(0, 0, 10);
-		m_steer.config_kP(0, 0.3, 10);
-		m_steer.config_kI(0, 0, 10);
-		m_steer.config_kD(0, 0, 10);
+
+        m_steer.configFactoryDefault();
+        m_steer.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 30);
+        m_steer.configNeutralDeadband(0.001, 30);
+		m_steer.config_kF(0, 0, 30);
+		m_steer.config_kP(0, 0.5, 30);
+		m_steer.config_kI(0, 0.01, 30);
+		m_steer.config_kD(0, 0.1, 30);
+        m_steer.configMotionCruiseVelocity(20000, 30);
+		m_steer.configMotionAcceleration(40000, 30);
+
+        m_drive.configFactoryDefault();
+        m_drive.configSelectedFeedbackSensor(TalonFXFeedbackDevice.IntegratedSensor, 0, 30);
+        m_drive.configNeutralDeadband(0.001, 30);
+		m_drive.config_kF(0, 0, 30);
+		m_drive.config_kP(0, 0.5, 30);
+		m_drive.config_kI(0, 0.01, 30);
+		m_drive.config_kD(0, 0.1, 30);
+        m_drive.configMotionCruiseVelocity(100, 30);
+		m_drive.configMotionAcceleration(400, 30);
     }
     
     public void drive(double power){
-        m_drive.set(TalonFXControlMode.PercentOutput, power * (inverted?-1.0:1.0));
+        if (power>1) {
+            power = 1;
+        }
+        m_drive.set(TalonFXControlMode.Velocity, power * 409.6);
     }
 
     //rotates to a position given in ticks
@@ -50,7 +69,7 @@ public class SwerveMod {
             destination = joystickTargetPlus;
         } else {
             destination = joystickTargetMinus;
-        }
+        }  
 
         if(Math.abs(destination-motorPos) > ticksPerRot/4.0){
             inverted = true;
@@ -62,7 +81,7 @@ public class SwerveMod {
             inverted = false;
         }
 
-        m_steer.set(TalonFXControlMode.Position, destination);
+        m_steer.set(TalonFXControlMode.MotionMagic, destination);
     }
 
 }
