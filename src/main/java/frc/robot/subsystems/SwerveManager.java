7e63f7d5b2b7ec981327ba328be0d479e090af42
ParkerHitch <parker.hitchcock@gmail.com>
2022-02-09 14:41:54 -0600

    SwerveManager Normalization
    Quick fix adding normalization to the manager so there won't be any funny business

--- src/main/java/frc/robot/subsystems/SwerveManager.java
+++ src/main/java/frc/robot/subsystems/SwerveManager.java
@@ -13,19 +13,25 @@ public class SwerveManager {
 
     public static void rotateAndDrive(double rotSpeed, double moveX, double moveY){
 
+        double[][] vectors = new double[m_swerveMods.length][2];
+        double max = 1.0;
+
         for(int i=0; i<m_swerveMods.length;i++){
             double x = moveX + -1*m_swerveMods[i].m_yPos*rotSpeed;
             double y = moveY + m_swerveMods[i].m_xPos*rotSpeed;
 
-            double direction = Math.atan2(y, x);
-            double power = Math.hypot(x, y);
+            vectors[i] = new double[]{x, y};
+            max = Math.max(max, Math.hypot(x, y));
+        }
+
+        for(int i=0; i<m_swerveMods.length;i++){
+            double direction = Math.atan2(vectors[i][1], vectors[i][0]);
+            double power = Math.hypot(vectors[i][0], vectors[i][1])/max;
             
             m_swerveMods[i].rotateToRad(direction);
             m_swerveMods[i].drive(power);
         }
 
     }
-
-
     
 }
