89ce92302601d77dc6caba2300f12bbd3b60aa3c
team3082-user <minnetonka3082@gmail.com>
2022-02-12 10:01:15 -0800

    rotateX and rotateY normalization fix
    Vectors were being individually normalized. This change should make it so that they're normalized according to the magnitude of the longest vector

--- src/main/java/frc/robot/subsystems/SwerveManager.java
+++ src/main/java/frc/robot/subsystems/SwerveManager.java
@@ -14,23 +14,28 @@ public class SwerveManager {
     }
 
     public static void rotateAndDrive(double rotSpeed, double moveX, double moveY) {
-
         // Array containing the unclamped movement vectors of each module
         double[][] vectors = new double[m_swerveMods.length][2];
         // The greatest speed of any of the modules. If any one module's speed is
         // greater than 1.0, all of the speeds are scaled down.
-        double max = 1.0;
+        double maxSpeed = 1.0;
+
+        // The greatest magnitude of any module's distance from the center of rotation
+        double maxModPosMagnitude = 0;
+        for (int i = 0; i < m_swerveMods.length; i++) {
+            maxModPosMagnitude = Math.max(maxModPosMagnitude,
+                    Math.hypot(m_swerveMods[i].m_yPos, m_swerveMods[i].m_xPos));
+        }
 
         // Calculate unclamped movement vectors
         for (int i = 0; i < m_swerveMods.length; i++) {
-            // The magnitude of the module's distance from the center of rotation
-            double modPosMagnitude = Math.hypot(m_swerveMods[i].m_yPos, m_swerveMods[i].m_xPos);
             // The vector representing the direction the module should move to achieve the
             // desired rotation. Calculated by taking the derivative of the module's
             // position on the circle around the center of rotation, normalizing the
-            // resulting vector, and scaling it by a factor of rotSpeed.
-            double rotateX = (-1 * m_swerveMods[i].m_yPos / modPosMagnitude) * rotSpeed;
-            double rotateY = (m_swerveMods[i].m_xPos / modPosMagnitude) * rotSpeed;
+            // resulting vector according to maxModPosMagnitude (such that the magnitude of
+            // the largest vector is 1), and scaling it by a factor of rotSpeed.
+            double rotateX = (-1 * m_swerveMods[i].m_yPos / maxModPosMagnitude) * rotSpeed;
+            double rotateY = (m_swerveMods[i].m_xPos / maxModPosMagnitude) * rotSpeed;
 
             // The final movement vector, calculated by summing movement and rotation
             // vectors
@@ -38,18 +43,19 @@ public class SwerveManager {
             double y = moveY + rotateY;
 
             vectors[i] = new double[] { x, y };
-            max = Math.max(max, Math.hypot(x, y));
+            maxSpeed = Math.max(maxSpeed, Math.hypot(x, y));
         }
 
         for (int i = 0; i < m_swerveMods.length; i++) {
             // Convert the movement vectors to a directions and magnitudes, clamping the
             // magnitudes based on 'max'
             double direction = Math.atan2(vectors[i][1], vectors[i][0]);
-            double power = Math.hypot(vectors[i][0], vectors[i][1]) / max;
+            double power = Math.hypot(vectors[i][0], vectors[i][1]) / maxSpeed;
 
             // Drive the swerve modules
-            m_swerveMods[i].rotateToRad(direction);
             m_swerveMods[i].drive(power);
+            if (Math.abs(power) > 0.1)
+                m_swerveMods[i].rotateToRad(direction);
         }
 
     }
